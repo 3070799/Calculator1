be5f7d34cc10d0fcc59ab592a79d52129c2e51dd
3070799 <3070799pp@gmail.com>
2020-11-21 03:12:02 +0200

    Исправил возможность вводить числа больше 10

--- src/Menu.java
+++ src/Menu.java
@@ -3,7 +3,7 @@ public class Menu {
         System.out.println("Введите математическую операцию(a + b, a - b, a * b, a / b)");
         System.out.println("Калькулятор подерживает числа на ввод от 0 до 9 также римские числа I-X");
         String value = Operation.readConsoleValue();
-        if (Check.checkOfArabic(value)) {
+        if (Check.checkOfArabic(value)&&Operation.fromStringToInt(value)[0]<= 10 && Operation.fromStringToInt(value)[1]<= 10) {
             if (value.indexOf('+') != -1) {
                 System.out.println(Operation.plus(Operation.fromStringToInt(value)));
             } else if (value.indexOf('-') != -1) {
