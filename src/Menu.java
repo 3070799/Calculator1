2571284253391e3a3f819fdcb6440ea23aa6b7b8
3070799 <3070799pp@gmail.com>
2020-11-21 09:25:43 +0200

    ошибки в тексте

--- src/Menu.java
+++ src/Menu.java
@@ -1,7 +1,7 @@
 public class Menu {
     public static void mainMenu() {
         System.out.println("Введите математическую операцию(a + b, a - b, a * b, a / b)");
-        System.out.println("Калькулятор подерживает числа на ввод от 0 до 9 также римские числа I-X");
+        System.out.println("Калькулятор подерживает числа на ввод от 0 до 10 также римские числа I-X");
         String value = Operation.readConsoleValue();
         if (Check.checkOfArabic(value)&&Operation.fromStringToInt(value)[0]<= 10 && Operation.fromStringToInt(value)[1]<= 10) {
             if (value.indexOf('+') != -1) {
@@ -16,7 +16,7 @@ public class Menu {
         }else if (Check.chekOfRoman(value)) {
             System.out.println(RomanFormat.operation(value));
         }else {
-            System.out.println("Неверно ведено выражение введите в формате([1-10][+,-,*,/][1-10] или [I-X][+,-,*,/][I-X]");
+            System.out.println("Неверно ведено выражение, введите в формате([1-10][+,-,*,/][1-10] или [I-X][+,-,*,/][I-X]");
             mainMenu();
         }
 
